33451605998131d2465c0f0469006f3960dfe6d1
Carin Nöjd <carinnojd@Carins-MacBook-Pro.local>
2016-05-08 19:00:53 +0200

    A start with the mobile-first design

---
+++ public/footer.php
@@ -0,0 +1,12 @@
+
+			<div class="footer">footer</div>
+
+		</div> <!-- /page-container -->
+	</div> <!-- /wrapper -->
+
+<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.3/TweenMax.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.3/plugins/ScrollToPlugin.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ public/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>Kajsas rosor</title>
+	<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-hQpvDQiCJaD2H465dQfA717v7lu5qHWtDbWNPvaTJ0ID5xnPUlVXnKzq7b8YUkbN" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+	<div class="wrapper">
+		<header class="header">
+			<div class="inner">
+				<div class="logo"><img src="../src/img/kajsas_rosor_logo.svg"></div>
+				<div class="mobile-toggle" id="mobile-toggle"><i class="fa fa-bars"></i></div>
+				
+			</div>
+				<div class="slogan">slogan</div>
+				<div class="menu-container" id="menu">
+					<ul>
+						<li>1</li>
+						<li>2</li>
+						<li>3</li>
+						<li>4</li>
+						<li>5</li>
+					</ul>
+				</div>
+		</header>
+		<div class="header_image"></div>
+		<div class="page-container">
+			<div class="page-title">Detta är Kajsas rosor!</div>
+
+
